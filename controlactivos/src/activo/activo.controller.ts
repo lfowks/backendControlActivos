12d8d4493d4a597460d221655338a0371acb2a42
unknown <jtorresjimenez2002@gmail.com>
2024-09-25 19:49:24 -0600

    sticker

--- controlactivos/src/activo/activo.controller.ts
+++ controlactivos/src/activo/activo.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Res } from '@nestjs/common';
 import { ActivoService } from './activo.service';
 import { UpdateActivoDTO } from './dto/update-activo.dto';
 import { Activo } from 'src/Entities/activo.entity';
 import { CreateActivoDTO } from './dto/create-activo.dto';
+import * as bwipjs from 'bwip-js';
+import { Response } from 'express';
 
 @Controller('activo')
 export class ActivoController {
@@ -39,4 +41,27 @@ export class ActivoController {
         return await this.activoService.deleteActivo(id);
     }
 
+    // Nuevo endpoint para generar el código de barras
+    @Get('barcode/:numPlaca')
+    async generateBarcode(@Param('numPlaca') numPlaca: string, @Res() res: Response) {
+        try {
+            bwipjs.toBuffer({
+                bcid: 'code128',       // Formato de código de barras (CODE128)
+                text: numPlaca,        // El número de placa del activo
+                scale: 3,              // Escala del código de barras
+                height: 10,            // Altura del código de barras
+                includetext: true,     // Incluir el texto del número de placa debajo del código de barras
+                textxalign: 'center',  // Alineación del texto
+            }, (err, png) => {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.setHeader('Content-Type', 'image/png');
+                    res.send(png);
+                }
+            });
+        } catch (error) {
+            res.status(500).send('Error generando el código de barras');
+        }
+    }
 }
